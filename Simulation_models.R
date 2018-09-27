af7bc7f2fac2d21809c745bb10333df896c0e417
Ellen Coombs <ellencoombs@ellens-air.home>
2018-09-27 13:15:05 +0100

    Running new GAMs

--- Simulation_models.R
+++ Simulation_models.R
@@ -35,5 +35,27 @@ Mortality <- Mortality %>%
 Turtle_strandings_2009 <- Turtle_strandings %>%
   filter(row_number() %in% 1:50)
 
+#=================================================================================================
+#Population 2009 
 
+Population_2009 <- Population %>%
+  filter(row_number() %in% 1:50)
 
+#=================================================================================================
+#Bind all the data together for modelling 
+#install.packages("tidyr") - run this when running the code for the first time 
+install.packages("tidyr")
+library(tidyr)
+
+#Ensure all correlates are read in using the above script 
+simulation_model <- bind_cols(Turtle_strandings_2009, Population_2009, GS_transport, NAC_transport)
+
+#Remove uneeded columns 
+simulation_model$Year1<- NULL
+simulation_model$Year2 <- NULL 
+simulation_model$Year <- NULL
+
+#Change the name of the SST variable, year and strandings count
+simulation_model <- simulation_model %>%
+  rename("Year" = YEAR) %>%
+  rename("Total_strandings" = "Count")
